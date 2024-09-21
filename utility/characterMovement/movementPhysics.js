122a399b89a693fc958518c9d8f7838260cead93
Lukas Jasiulionis <lukas.jasiulionis@mif.stud.vu.lt>
2024-09-21 13:27:53 +0300

    Index name changes and file changes

--- utility/characterMovement/movementPhysics.js
+++
@@ -1,9 +0,0 @@
-export class MovementPhysics{
-    //greitis kuri characteris gali pasiekti max (pixeliais per tick)
-    static maxSpeed = 10;
-
-    static calculateSpeed(movementMomentum, reduceDiagonalSpeed){
-        return MovementPhysics.maxSpeed * (movementMomentum * reduceDiagonalSpeed);
-    }
-
-}
