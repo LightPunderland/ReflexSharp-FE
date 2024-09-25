d3293c9103e70c95c0b37da4dc9cbe9031e6f196
burbalas <edgaras.burbulis@gmail.com>
2024-09-25 14:45:17 +0300

    Despawnina projectiles
    Padariau, kad despawnintu projectiles kurie yra uz ekrano ribu

--- game/main.js
+++ game/main.js
@@ -56,8 +56,14 @@ app.ticker.add((ticker) => {
     character.update(ticker.deltaTime);
 
     //update'ina visus sviedinius
-    projectiles.forEach((projectile) => {
-        projectile.update();
+    projectiles = projectiles.filter((projectile) => {
+
+        // Tikrina ar projectile akttyvus
+        if (projectile.update()) {
+            return false; 
+        }
+        return true;
+        
     });
 });
 
