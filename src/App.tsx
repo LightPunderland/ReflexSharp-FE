c19cff476bbbcf6216d5341f1feaddf440c0b2d1
Lukas Jasiulionis <lukas.jasiulionis@mif.stud.vu.lt>
2024-09-28 13:19:24 +0300

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Login from './Login/Login'
+import Navbar from './Navbar/Navbar'
+
+function App() {
+ 
+  return (
+    <>
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Login/Login.tsx
@@ -0,0 +1,12 @@
+import { GoogleOAuthProvider } from "@react-oauth/google"
+
+
+function Login(){
+
+    return(
+        <div>"Hi"</div>
+    );
+}
+
+
+export default Login;
