50159da77d176db887430ccefee755408571a7a6
Kipras Kazarinas <kipras.kazarinas@mif.stud.vu.lt>
2024-09-30 21:45:59 +0300

    Prideti nauji assets, su jais paupdate'intas meniu.

--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -1,21 +1,39 @@
 import { NavLink } from "react-router-dom";
 import styles from "./Navbar.module.css";
+import playImage from "../assets/play.png"; // Adjust the path as necessary
+import profileImage from "../assets/profile.png"; // Adjust the path as necessary
+import leaderboardImage from "../assets/leaderboard.png"; // Adjust the path as necessary
+import loginImage from "../assets/login.png";
 
-
-function Navbar(){
-
-    return(<header className={styles.header}>
-                <nav className={styles['navbar-container']} id="main-bar">
-                       
-                    <ul className={styles['navbar-list']}>
-                        <li><NavLink to="/play"> Play </NavLink></li>
-                        <li><NavLink to="/profile"> Profile </NavLink></li>
-                        <li><NavLink to="/leaderboard"> Leaderboard </NavLink></li>
-                        <li><NavLink to="/login"> Login </NavLink></li>
-                    </ul>
-                    
-                </nav>
-            </header>);
+function Navbar() {
+    return (
+        <header className={styles.header}>
+            <nav className={styles['navbar-container']} id="main-bar">
+                <ul className={styles['navbar-list']}>
+                    <li>
+                        <NavLink to="/play">
+                            <img src={playImage} alt="Play" />
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/profile">
+                            <img src={profileImage} alt="Profile" />
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/leaderboard">
+                            <img src={leaderboardImage} alt="Leaderboard" />
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/login">
+                            <img src={loginImage} alt="Login" />
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
 }
 
 export default Navbar;
