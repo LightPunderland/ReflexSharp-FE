3ee3f53fa7a516cc4e46e754e516548ea3c71e54
EimantasGag <eimis.gagilas@gmail.com>
2024-11-13 16:35:49 +0200

    Setupinau testu enviroment

---
+++ tests/character.test.ts
@@ -0,0 +1,11 @@
+import { expect, test } from 'vitest'
+import { Character } from '../src/Play/utility/character'
+
+test('Initial number of keys pressed', () => {
+    // Kazkokie character testai
+})
+
+// test('Initial number of keys pressed', () => {
+//     KeyboardKeys.onKeyDown({altKey})
+//     expect(KeyboardKeys.numberOfKeysPressed()).toBe(0)
+// })
---
+++ tests/keyboardKeys.test.ts
@@ -0,0 +1,12 @@
+import { expect, test } from 'vitest'
+import { KeyboardKeys } from '../src/Play/utility/keyboardKeys'
+import { KeyboardEvent } from 'react'
+
+test('Initial number of keys pressed', () => {
+    expect(KeyboardKeys.numberOfKeysPressed()).toBe(0)
+})
+
+// test('Key pressed', () => {
+//     KeyboardKeys.onKeyDown({altKey})
+//     expect(KeyboardKeys.numberOfKeysPressed()).toBe(0)
+// })
