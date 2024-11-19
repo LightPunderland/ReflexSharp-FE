0e2b83a6588b03cf9248d0e06c71390fa6bbfeaa
EimantasGag <eimis.gagilas@gmail.com>
2024-11-19 22:01:18 +0200

    Testu enviroment

--- tests/character.test.ts
+++ tests/character.test.ts
@@ -2,6 +2,8 @@ import { expect, test } from 'vitest'
 import { Character } from '../src/Play/utility/character'
 
 test('Initial number of keys pressed', () => {
+    const c = new Character()
+    expect(c.checkForOutOfBounds()).toBe(null);
     // Kazkokie character testai
 })
 
