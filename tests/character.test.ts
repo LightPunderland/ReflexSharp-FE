891eb88ba6872da70fc0ee8f3a6894818ad52886
EimantasGag <eimis.gagilas@gmail.com>
2024-11-20 20:23:02 +0200

    Setupinta testu enviroment

---
+++ tests/character.test.ts
@@ -0,0 +1,15 @@
+import { expect, test, vi } from 'vitest'
+import { Character } from '../src/Play/utility/character'
+import { SpriteCache } from '../src/Play/utility/spriteCache';
+
+function getC() {
+    const spriteCache = SpriteCache.instance;
+    const c = new Character();
+    c.checkForOutOfBounds();
+  }
+
+test('Initial number of keys pressed', () => {
+    const mock = vi.fn().mockImplementation(getC);
+
+    expect(mock()).toEqual(null);
+})
