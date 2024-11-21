570cc5680770ee4945daf449f4c85e8a3800b951
EimantasGag <eimis.gagilas@gmail.com>
2024-11-21 12:21:31 +0200

    Dar testu (☞ﾟヮﾟ)☞☜(ﾟヮﾟ☜)

--- tests/keyboardKeys.test.ts
+++ tests/keyboardKeys.test.ts
@@ -1,7 +1,20 @@
 import { expect, test } from 'vitest'
 import { KeyboardKeys } from '../src/Play/utility/keyboardKeys'
-import { KeyboardEvent } from 'react'
 
 test('Initial number of keys pressed', () => {
     expect(KeyboardKeys.numberOfKeysPressed()).toBe(0)
+})
+
+test('Keyboard key pressed', () => {
+    KeyboardKeys.keyboardState["keyA"] = true;
+    expect(KeyboardKeys.numberOfKeysPressed()).toBe(1);
+})
+
+test('Any key pressed', () => {
+    KeyboardKeys.keyboardState["keyA"] = true;
+    KeyboardKeys.keyboardState["keyW"] = true;
+    KeyboardKeys.keyboardState["keyS"] = true;
+    KeyboardKeys.keyboardState["keyD"] = true;
+
+    expect(KeyboardKeys.anyKeyPressed()).toBe(true);
 })
