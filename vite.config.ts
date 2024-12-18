dd48df8b381a5802380af5bb1e70274b9a1bd3b2
Kipras Kazarinas <kipras.kazarinas@mif.stud.vu.lt>
2024-12-18 10:57:53 +0200

    background is back

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
 
     proxy: {
       '/host': {
-        target: 'https://lukasjasiulionis.lt',
+        target: 'http://localhost:5050',
         changeOrigin: true 
       }
     }
